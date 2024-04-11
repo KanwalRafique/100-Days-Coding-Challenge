264e3f3f9124af8842f704357f27365f4246da8a
KanwalRafique <160254148+KanwalRafique@users.noreply.github.com>
2024-04-11 17:06:37 +0500

    Add files via upload

---
+++ Day 02 challenge/Q4.ts
@@ -0,0 +1,12 @@
+//Question 2: Famous Quote 2: 
+//Repeat Exercise 1, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_person: string = "Muhammad Iqbal";
+
+let famousquote: string ='"The ultimate aim of the ego is not to see something, but to be something."'
+
+// compose message
+
+let message: string = `${famous_person} once said, ${famousquote}`
+
+console.log(message);
---
+++ Day 02 challenge/Q5.js
@@ -0,0 +1,8 @@
+"use strict";
+// Question 3: Stripping Names: 
+// Store a person’s name, and include some whitespace characters at the beginning and end of the name. Use "\t" (tab) and "\n" (new line) at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after stripping the white spaces.
+Object.defineProperty(exports, "__esModule", { value: true });
+let firstName = "Kanwal\tRafique";
+let firstName1 = "Kanwal\nRafique";
+console.log(firstName);
+console.log(firstName1);
